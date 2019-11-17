c810f309c43894e9fc8626572a83a6f415bc3e16
Iikka Hauhio <iikka@kaivos.org>
2019-11-18 01:51:35 +0200

    generating random characters

--- src/gurps_hahmotin/generator.clj
+++ src/gurps_hahmotin/generator.clj
@@ -18,27 +18,35 @@
           (get options (dec c))
           (recur))))))
 
-(defn prompt-optional-CP [options]
-  (prompt options :formatter #(str/join ", " (for [[skill cp] %]
-                                               (str (c/render-name skill) " " cp)))))
+(defn random-prompter [options & o]
+  (rand-nth options))
 
-(defn prompt-template [templates]
-  (let [choice (prompt (conj templates {:name "None"}) :formatter :name)]
+(defn prompt-optional-CP [options & {:keys [prompter] :or {prompter prompt}}]
+  (prompter options :formatter #(str/join ", " (for [[skill cp] %]
+                                                 (str (c/render-name skill) " " cp)))))
+
+(defn prompt-template [templates & {:keys [prompter] :or {prompter prompt}}]
+  (let [choice (prompter (conj templates {:name "None"}) :formatter :name)]
     (as-> (get choice :used-CP {}) used-CP
-      (reduce #(merge-with + %1 (prompt-optional-CP %2)) used-CP (:optional-CP choice))
+      (reduce #(merge-with + %1 (prompt-optional-CP %2 :prompter prompter))
+              used-CP
+              (:optional-CP choice))
       (if (empty? (:lens choice))
         used-CP
-        (merge-with + used-CP (prompt-template (:lens choice)))))))
+        (merge-with + used-CP (prompt-template (:lens choice) :prompter prompter))))))
       
 
-(defn prompt-character []
+(defn prompt-character [& {:keys [prompter] :or {prompter prompt}}]
   (merge-with +
-              (prompt-template (vals t/species))
-              (prompt-template (vals t/backgrounds))
-              (prompt-template (vals t/professions))))
+              (prompt-template (vals t/species) :prompter prompter)
+              (prompt-template (vals t/backgrounds) :prompter prompter)
+              (prompt-template (vals t/professions) :prompter prompter)))
 
-(defn -main [outfile]
-  (let [used-CP (prompt-character)
+(defn -main [outfile & flags]
+  (let [flags-set (set flags)
+        used-CP (prompt-character :prompter (if (contains? flags-set "--random")
+                                              random-prompter
+                                              prompt))
         character {:used-CP used-CP}
         html (c/render-character-sheet character)]
     (prn character)
